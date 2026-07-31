4bf22871be74fcff7f6e5495966f5a5f093bb5e9
Víctor Luévano <victoradrian_2003@outlook.com>
2026-07-31 12:11:20 -0600

    Corrección de URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
